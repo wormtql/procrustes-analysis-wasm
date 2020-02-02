278e5deb71c426ebc1c73fd00f7a403f1da430aa
IATOW <584130248@qq.com>
2020-02-02 20:54:13 +0800

    add pkg

---
+++ wasm/rust-wasm/pkg/rust_wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function greet(): void;
+/**
+* @param {Float64Array} arr 
+* @param {number} shapes 
+* @param {number} dots 
+* @returns {Float64Array} 
+*/
+export function calc(arr: Float64Array, shapes: number, dots: number): Float64Array;
---
+++ wasm/rust-wasm/pkg/rust_wasm.js
@@ -0,0 +1,74 @@
+import * as wasm from './rust_wasm_bg.wasm';
+
+const lTextDecoder = typeof TextDecoder === 'undefined' ? require('util').TextDecoder : TextDecoder;
+
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachegetUint8Memory0 = null;
+function getUint8Memory0() {
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+/**
+*/
+export function greet() {
+    wasm.greet();
+}
+
+let cachegetFloat64Memory0 = null;
+function getFloat64Memory0() {
+    if (cachegetFloat64Memory0 === null || cachegetFloat64Memory0.buffer !== wasm.memory.buffer) {
+        cachegetFloat64Memory0 = new Float64Array(wasm.memory.buffer);
+    }
+    return cachegetFloat64Memory0;
+}
+
+let WASM_VECTOR_LEN = 0;
+
+function passArrayF64ToWasm0(arg, malloc) {
+    const ptr = malloc(arg.length * 8);
+    getFloat64Memory0().set(arg, ptr / 8);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+
+let cachegetInt32Memory0 = null;
+function getInt32Memory0() {
+    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory0;
+}
+
+function getArrayF64FromWasm0(ptr, len) {
+    return getFloat64Memory0().subarray(ptr / 8, ptr / 8 + len);
+}
+/**
+* @param {Float64Array} arr
+* @param {number} shapes
+* @param {number} dots
+* @returns {Float64Array}
+*/
+export function calc(arr, shapes, dots) {
+    var ptr0 = passArrayF64ToWasm0(arr, wasm.__wbindgen_malloc);
+    var len0 = WASM_VECTOR_LEN;
+    wasm.calc(8, ptr0, len0, shapes, dots);
+    var r0 = getInt32Memory0()[8 / 4 + 0];
+    var r1 = getInt32Memory0()[8 / 4 + 1];
+    var v1 = getArrayF64FromWasm0(r0, r1).slice();
+    wasm.__wbindgen_free(r0, r1 * 8);
+    return v1;
+}
+
+export const __wbg_alert_d87a18f2b03e82aa = function(arg0, arg1) {
+    alert(getStringFromWasm0(arg0, arg1));
+};
+
